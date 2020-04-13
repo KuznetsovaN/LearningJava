3afe6d6a61220b6e5e0a97f264d6c83699a97064
Welest <welest89@gmail.com>
2020-04-14 00:27:58 +0300

    Задание 10 исправление

--- src/task10.java
+++ src/task10.java
@@ -1,23 +1,33 @@
 import java.util.Scanner;
 
 //Произведите ввод данных с клавиатуры в матрицу, а после этого произведите вывод первой строки матрицы на экран, где каждый элемент умножается на 3. Размерность матрицы задается пользователем.
+
 public class task10 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in); // Объявляем Scanner
-        System.out.println("Enter array length: ");
+        System.out.println("Ввести количество строк: ");
         int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-        int array[][] = new int[size][size]; // Создаём массив int размером в size
-        System.out.println("Insert array elements:");
-        /*Пройдёмся по всему массиву, заполняя его*/
+        System.out.println("Ввести количество столбцов: ");
+        int size1 = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size1
+        int array[][] = new int[size][size1]; // Создаём массив int размером в size
+        System.out.println("Заполняем элеметами матрицу:");
+//Пройдёмся по всему массиву, заполняя его
+
         for (int t = 0; t < size; t++) {
-            for (int i = 0; i < size; i++) {
+            System.out.print("ввести строки "+(t+1)+" " ); // Выводим на экран, полученный массив {
+            for (int i = 0; i < size1; i++) {
                 array[t][i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
             }
         }
+      int t = 0;
+            for (int i = 0; i < size1; i++) {
+                array[t][i]= array[t][i]*3; // умножаем на три
+            }
+
             System.out.print("Inserted array elements:");
-            for (int t = 0; t < size; t++) {
-                for (int i = 0; i < size; i++) {
-                    System.out.print(" " + array[t][i] * 3); // Выводим на экран, полученный массив
+            for (t = 0; t < size; t++) {
+                for (int i = 0; i < size1; i++) {
+                    System.out.print(" " + array[t][i] ); // Выводим на экран, полученный массив
                 }
                 System.out.println();
             }
